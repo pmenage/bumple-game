3db90261e7daf436db849bd0f64a1f7cd1cafae8
pmenage <pauline.menage26@gmail.com>
2017-01-21 13:30:38 +0100

    Added sliding obstacles

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,8 +1,11 @@
+import processing.core.PConstants;
+
 public class Obstacle {
 
     private float height = 0.15f;
     private float width = 1f;
-    private Point<Float> position = new Point<Float>(0f,0f);
+    private float duration;
+    private Point<Float> position = new Point<>(0f,0f);
     private ObstacleType type;
 
     public enum ObstacleType {
@@ -10,14 +13,22 @@ public class Obstacle {
         Rotating
     }
 
-    public Obstacle(ObstacleType type, float y) {
+    public Obstacle(ObstacleType type, float y, float duration) {
         this.type = type;
         position.y = y;
+        this.duration = duration;
         if (type == ObstacleType.Sliding)
-            position.x = 0.25f;
+            position.x = -2 * width;
+    }
+
+    public void update() {
+        if (position.x < -width)
+            position.x += width / (duration * 60);
     }
 
     public void draw(Bumple bumple) {
+        update();
+        bumple.rectMode(PConstants.CORNER);
         Point<Integer> positionPixels = bumple.metersToPixels(position);
         bumple.rect(positionPixels.x, positionPixels.y, bumple.metersToPixels(width), bumple.metersToPixels(height));
     }
