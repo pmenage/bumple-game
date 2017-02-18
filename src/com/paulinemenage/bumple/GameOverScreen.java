149a0c11617323198788b4585747031481d2cb21
pmenage <pauline.menage26@gmail.com>
2017-02-18 22:17:31 +0100

    Added game over image

--- src/com/paulinemenage/bumple/GameOverScreen.java
+++ src/com/paulinemenage/bumple/GameOverScreen.java
@@ -2,14 +2,17 @@ package com.paulinemenage.bumple;
 
 import com.paulinemenage.bumple.game.Bumple;
 import processing.core.PConstants;
+import processing.core.PImage;
 import processing.event.KeyEvent;
 
 public class GameOverScreen extends Screen {
 
     private Applet applet;
+    private PImage gameOver;
 
     public GameOverScreen(Applet applet) {
         this.applet = applet;
+        gameOver = applet.loadImage("game-over.png");
     }
 
     /**
@@ -30,10 +33,12 @@ public class GameOverScreen extends Screen {
     @Override
     public void draw() {
         applet.clear();
+        applet.background(255, 255, 255);
+        applet.imageMode(PConstants.CENTER);
+        applet.image(gameOver, 200, 170, 380, 214);
         applet.textAlign(PConstants.CENTER);
-        applet.textSize(30);
-        applet.text("Game over", 200, 170);
         applet.textSize(20);
+        applet.fill(50);
         applet.text("Press m to return to menu", 70, 250, 100, 200);
         applet.text("Press spacebar to play again", 200, 250, 150, 200);
     }
