50e2a4b0eba12d3cd54462af80c308e3bf800955
pmenage <pauline.menage26@gmail.com>
2017-02-19 19:39:10 +0100

    Reset bubbles when they leave the frame

--- src/com/paulinemenage/bumple/game/BubbleGenerator.java
+++ src/com/paulinemenage/bumple/game/BubbleGenerator.java
@@ -8,24 +8,36 @@ public class BubbleGenerator {
 
     private class Bubble {
 
-        public Circle circle;
+        public Circle circle = new Circle(new Point(0, 0), 0);
         public float vy;
 
-        public Bubble(Point center, float radius, float vy) {
+        /*public Bubble(Point center, float radius, float vy) {
             circle = new Circle(center, radius);
             this.vy = vy;
-        }
+        }*/
 
     }
 
-    private static final int STOCK = 15;
-
-    private int numberOfBubbles = STOCK;
-    private Bubble[] bubbles = new Bubble[STOCK];
+    private Bubble[] bubbles = new Bubble[15];
 
     public BubbleGenerator() {
-        for (int i = 0; i < STOCK; i++)
-            bubbles[i] = new Bubble(new Point((float) Math.random() * 2, 3.1f), (float) Math.random() * .2f, (float) Math.random());
+        for (int i = 0; i < bubbles.length; i++) {
+            bubbles[i] = new Bubble();
+            resetBubble(bubbles[i]);
+        }
+    }
+
+    public void resetBubble(Bubble bubble) {
+        bubble.circle.center.x = (float) Math.random() * 2;
+        bubble.circle.center.y = 3.1f;
+        bubble.circle.radius = (float) Math.random() * .2f;
+        bubble.vy = (float) Math.random();
+    }
+
+    public void regenerateBubble() {
+        for (Bubble bubble : bubbles)
+            if (bubble.circle.center.y < -0.1)
+                resetBubble(bubble);
     }
 
     public void draw(Bumple bumple, PApplet pApplet) {
@@ -43,6 +55,7 @@ public class BubbleGenerator {
     public void update(float delta) {
         for (Bubble bubble : bubbles)
             bubble.circle.center.y -= bubble.vy * delta;
+        regenerateBubble();
     }
 
 }
